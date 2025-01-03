4b897ec4c526b7242be7f2ae168182104e943d69
Marcos Pérez <79529071+mpf-engineer@users.noreply.github.com>
2025-01-03 01:01:12 +0100

    Creación Modulo MatrixIO e implementar mostrarMatriz

---
+++ core/src/main/java/es/uca/mps/devops/MatrixIO.java
@@ -0,0 +1,12 @@
+package es.uca.mps.devops;
+
+public class MatrixIO {
+    public static void mostrarMatriz(int[][] matriz) {
+        for (int[] fila : matriz) {
+            for (int elemento : fila) {
+                System.out.print(elemento + " ");
+            }
+            System.out.println();
+        }
+    }
+}
