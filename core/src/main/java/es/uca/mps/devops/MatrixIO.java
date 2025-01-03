d8a7790b9f201733eeae6585145339cb8431852e
FedeLo <fede.lopezpe@alum.uca.es>
2025-01-03 11:36:13 +0100

    Creación del método cargarMatriz

--- core/src/main/java/es/uca/mps/devops/MatrixIO.java
+++ core/src/main/java/es/uca/mps/devops/MatrixIO.java
@@ -1,6 +1,12 @@
 package es.uca.mps.devops;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MatrixIO {
+    private static final int N = 5;
+
     public static void mostrarMatriz(int[][] matriz) {
         for (int[] fila : matriz) {
             for (int elemento : fila) {
@@ -9,4 +15,20 @@ public class MatrixIO {
             System.out.println();
         }
     }
+
+     public static boolean cargarMatriz(String nombreArchivo, int[][] matriz) {
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            for (int i = 0; i < N; i++) {
+                String[] elementos = br.readLine().trim().split("\\s+");
+                for (int j = 0; j < N; j++) {
+                    matriz[i][j] = Integer.parseInt(elementos[j]);
+                }
+            }
+            return true;
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("Error al cargar el archivo: " + e.getMessage());
+            return false;
+        }
+    }
 }
+
